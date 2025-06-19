85c7e24a14fa1a2a865b3134fabeb32b571733aa
almamutuguti <alma.wanjiru8j@gmail.com>
2025-06-19 09:25:14 +0300

    add booleans

--- booleans.js
+++ booleans.js
@@ -2,3 +2,19 @@ let isLoggedIn = true;
 let hasAdminAccess = false;
 
 // === strict equality
+console.log(5 === '5'); // === strict equality
+console.log(5 == '5'); // == loose equality
+console.log(5 !== '5'); // !== strict inequality
+console.log(5 != '5'); // != loose inequality
+
+console.log(5 > 2);
+console.log(5 < 9);
+console.log(5 >= 5);
+console.log(5 <= 2);
+
+let fname;
+console.log(fname); // undefined
+
+let user = null;
+console.log(user);
+
